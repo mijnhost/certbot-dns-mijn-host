ac7e208984638aec650186f60ebb7c13184c928e
bllfr0g <39714379+bllfr0g@users.noreply.github.com>
2024-09-02 22:37:32 -0700

    default_prop

--- certbot_dns_mijn_host/mijn_host.py
+++ certbot_dns_mijn_host/mijn_host.py
@@ -22,7 +22,7 @@ class Authenticator(dns_common.DNSAuthenticator):
 
     @classmethod
     def add_parser_arguments(
-        cls, add: Callable[..., None], default_propagation_seconds: int = 10
+        cls, add: Callable[..., None], default_propagation_seconds: int = 60
     ) -> None:
         super().add_parser_arguments(add, default_propagation_seconds)
         add("credentials", help="mijn.host credentials INI file.")
